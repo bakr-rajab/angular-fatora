4c39273b5afe557900b4b99c0b2c56f8bb79ff56
bakr-rajab <abobakr.r.bakry@gmail.com>
2023-09-28 13:21:55 +0300

    review company

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -9,17 +9,21 @@ declare function paggnation(): any;
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
-  styleUrls: ['./company.component.css']
+  styleUrls: ['./company.component.css'],
 })
 export class CompanyComponent implements OnInit {
   activityList: any[] = [];
 
-  companyModel: Company = new Company()
+  companyModel: Company = new Company();
   activityId!: string;
-  companyList: any
-  response: any
-  initTable: boolean = false
-  constructor(private staticSer: StaticService, private apiCall: CompanyService, private cd: ChangeDetectorRef) { }
+  companyList: any;
+  response: any;
+  initTable: boolean = false;
+  constructor(
+    private staticSer: StaticService,
+    private apiCall: CompanyService,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     sidebarToggling();
@@ -27,55 +31,61 @@ export class CompanyComponent implements OnInit {
   }
 
   getAll() {
-    this.apiCall.getAll().subscribe(res => {
-      this.response = res
+    this.apiCall.getAll().subscribe((res) => {
+      this.response = res;
       this.companyList = this.response;
       if (this.initTable == false) {
         paggnation();
         this.initTable = true;
       }
-    })
+    });
   }
 
   deleteCompany(id: string) {
-    this.apiCall.deleteCompany(id).subscribe(res => {
+    this.apiCall.deleteCompany(id).subscribe((res) => {
       console.log(res);
-      this.cd.detectChanges()
+      this.cd.detectChanges();
       if (res.affected == 1) {
-        this.companyList = this.companyList.filter((item: any) => item.id != id);
+        this.companyList = this.companyList.filter(
+          (item: any) => item.id != id
+        );
         console.log(this.companyList);
       }
-    })
-    this.cd.detectChanges()
+    });
+    this.cd.detectChanges();
   }
 
   addCompany() {
     console.log(this.companyModel);
-    this.apiCall.createCompany(this.companyModel).subscribe(res => {
-      if (res)
-        this.companyList = [...this.companyList, res];
-    })
+    this.apiCall.createCompany(this.companyModel).subscribe((res) => {
+      if (res) this.companyList = [...this.companyList, res];
+    });
   }
   setToEdit(company: Company) {
-    this.activityId = company.activity?.id
-    console.log();
+    this.activityId = company.activity?.id;
+    delete company.branch;
+    delete company.license;
     this.companyModel = company;
+    console.log('777777777777777', this.companyModel);
   }
   getActiviteis() {
-    this.staticSer.getActivity().subscribe(act => {
-      this.activityList = act
-    })
+    this.staticSer.getActivity().subscribe((act) => {
+      this.activityList = act;
+    });
   }
   edit() {
-    this.companyModel.activity = this.activityId
+    this.companyModel.activity = this.activityId;
     console.log(this.companyModel);
-    const { id, ...reset } = this.companyModel
-    this.apiCall.edit(id, reset).subscribe(res => {
+    const { id, ...reset } = this.companyModel;
+    this.apiCall.edit(id, reset).subscribe((res) => {
       console.log(res);
       if (res.affected == 1) {
-        this.getAll()
+        this.getAll();
       }
-    })
+    });
+  }
 
+  resetForm() {
+    this.companyModel = new Company();
   }
 }
