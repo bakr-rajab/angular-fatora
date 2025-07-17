7d709fbb6f5498b018b51de3cbe6b6cb4288f95d
bakr-rajab <abobakr.r.bakry@gmail.com>
2025-07-17 08:58:49 +0300

    fix: update unit selection to use unit code and change API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import { HttpHeaders } from "@angular/common/http";
 export const environment = {
   production: true,
   // https://splendid-tan-python.cyclic.app
-  apiUrl: 'https://worrisome-fedora-crab.cyclic.app',
+  apiUrl: 'https://envoice-nest-app.onrender.com',
   headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
